4a5a7e438cfb3140118cdd7a4241da33992854f3
Antonio Mario Henriques Filho <amhfilho@gmail.com>
2019-05-27 08:48:49 -0300

    LineReader

--- tdilogreader/src/main/java/com/ibm/integrationservices/App.java
+++ tdilogreader/src/main/java/com/ibm/integrationservices/App.java
@@ -1,6 +1,7 @@
 package com.ibm.integrationservices;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Hello world!
@@ -8,19 +9,79 @@ import java.io.IOException;
 public class App {
     public static void main(String[] args) throws IOException {
 
+        String eventId = "1985415";
+
         String file = "C:\\Users\\AntonioMarioHenrique\\Desktop\\temp\\chubb.prd.2018-12-04.log";
+        String command = "";
+        StringBuilder inputXml = new StringBuilder();
+        StringBuilder transformedCreateXML = new StringBuilder();
 
         System.out.println("opening file...");
-        long start = System.currentTimeMillis();
-        TdiFileReader reader = TdiFileReader.open(file);
+        List<String> lines = TdiFileReader.open(file).lines();
 
-        long time = System.currentTimeMillis()-start;
-        //reader.lines().forEach(n -> System.out.println(n));
+        for (int i = 0; i < lines.size(); i++) {
+            if (lines.get(i).contains("XPath command") &&
+                    lines.get(i).contains(eventId)) {
 
-        System.out.println(String.format("opened in %d milliseconds",time));
+                System.out.println(String.format("Line %s: %s" , i,lines.get(i)));
+                if(lines.get(i).contains("CREATE_PROBLEM")){
+                    command = "CREATE";
+                }
+                else if(lines.get(i).contains("UPDATE_PROBLEM")){
+                    command = "UPDATE";
+                }
+                else if(lines.get(i).contains("UPDATE_CALLBACK")){
+                    command = "UPDATE_CALLBACK";
+                }
 
-        TransactionSearch search = new TransactionSearch(reader.lines());
-        TdiTransaction transaction = search.findFirst("");
-        System.out.println(transaction);
+                boolean xmlFound = false;
+
+                while(!xmlFound){
+                    String line = lines.get(i++);
+                    if(line.contains("Input XML") && line.contains(eventId)){
+                        boolean endXml = false;
+                        while(!endXml){
+
+                            inputXml.append(line);
+                            if(line.contains("</command>")){
+                                endXml = true;
+                            }
+                            line = lines.get(i++);
+                        }
+                        xmlFound = true;
+                    }
+
+                }
+
+                boolean transformedCreateXmlFound = false;
+                while(!transformedCreateXmlFound) {
+                    String line = lines.get(i++);
+                    if (line.contains("transformedCreateXML") && line.contains(eventId)) {
+                        transformedCreateXmlFound = true;
+                        boolean createXMLFound = false;
+                        while (!createXMLFound) {
+                            transformedCreateXML.append(line);
+                            if (line.contains("</soapenv:Envelope>")) {
+                                createXMLFound = true;
+                            }
+                            line = lines.get(i++);
+                        }
+                    }
+                }
+
+
+            }
+        }
+
+        System.out.println(command);
+        String inputStringXml = inputXml.toString();
+        int start = inputStringXml.indexOf("<command");
+        String xml = inputStringXml.substring(start);
+        System.out.println(xml);
+
+        String createXml = transformedCreateXML.toString();
+        start = createXml.indexOf("<?xml");
+        createXml = createXml.substring(start);
+        System.out.println(createXml);
     }
 }
